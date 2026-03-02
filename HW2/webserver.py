0b36cac52e8954b3d3f00b1fba98ea9d036553ca
yyaatt0 <yathien122@gmail.com>
2026-03-01 18:45:31 -0800

    hw stuff

---
+++ HW2/webserver.py
@@ -0,0 +1,52 @@
+# import socket module
+from socket import *
+import sys # In order to terminate the program
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# Prepare a sever socket
+# Fill in start
+serverPort = 6789
+serverSocket.bind(("", serverPort))
+serverSocket.listen(1)
+# Fill in end
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() # Fill in start  # Fill in end
+    try:
+        message = connectionSocket.recv(1024).decode() # Fill in start # Fill in end
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read() # Fill in start # Fill in end
+        f.close()
+        
+        # Send one HTTP header line into socket
+        # Fill in start
+        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+        connectionSocket.send("Content-Type: text/html\r\n".encode())
+        connectionSocket.send("\r\n".encode())  
+        # Fill in end
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+    except IOError:
+        pass
+        # Send response message for file not found
+        # Fill in start
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+        connectionSocket.send("Content-Type: text/html\r\n".encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+        # Fill in end
+        
+        # Close client socket
+        # Fill in start
+        connectionSocket.close()
+        # Fill in end
+
+serverSocket.close()
+sys.exit() # Terminate the program after sending the corresponding data
